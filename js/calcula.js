73af306a235fe2fb169029a1a4a8793414181963
Diego Augusto dos Santos <diegoaugustods1993@gmail.com>
2020-02-05 17:00:24 -0300

    Alterei o nome do arquivo e criei funções para facilitar a leitura do código

---
+++ js/calcula.js
@@ -0,0 +1,43 @@
+let titulo = document.querySelector(".titulo");
+titulo.textContent = "Haha";
+
+let pacientes = document.querySelectorAll(".paciente");
+
+for(let i = 0; i < pacientes.length; i++){
+
+let paciente = pacientes[i];
+let tdPeso = paciente.querySelector(".info-peso");
+
+let peso = tdPeso.textContent;
+let tdAltura = paciente.querySelector(".info-altura");
+let altura = tdAltura.textContent;
+let tdImc = paciente.querySelector(".info-imc");
+
+let pesoValido = true;
+let alturaValida = true;
+    if(peso < 0 || peso > 1000){
+        console.log("Peso Invalido");
+        pesoValido = false;
+        tdImc.textContent = ("Peso Invalido");
+        paciente.classList.add("paciente-invalido");
+}
+
+    if (altura < 0 || altura > 1000){
+        console.log("invalida");
+        alturaValida = false;
+        tdAltura.textContent = ("Invalida")
+        paciente.classList.add("paciente-invalido");
+}
+
+    if(altura && pesoValido){
+        let imc = calcularImc(peso, altura);
+        tdImc.textContent = imc;
+}
+}
+
+function calcularImc(peso, altura){
+   
+    let imc = 0;
+    imc = peso / (altura* altura);
+    return imc.toFixed(2);
+}
